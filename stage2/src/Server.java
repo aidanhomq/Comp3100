3943f8a32e055e62fcd22c5512294e673dc05a3f
aidanhomq <aidan.ho@students.mq.edu.au>
2021-05-30 22:46:02 +1000

    addlist

---
+++ stage2/src/Server.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Server{
+    String serverType;
+    int serverID;
+    int state; 
+    int curStartTime;
+    int core;
+    int mem;
+    long disk;
+    int wJobs;
+    int rJobs;
+    
+}
