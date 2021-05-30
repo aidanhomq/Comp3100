13b1ba03291dc9c3a948ff83ff41e2459bbdc143
aidanhomq <aidan.ho@students.mq.edu.au>
2021-05-30 22:47:31 +1000

    AddServer_Info

--- stage2/src/Server.java
+++ stage2/src/Server.java
@@ -10,5 +10,33 @@ public class Server{
     long disk;
     int wJobs;
     int rJobs;
-    
+
+    public Server(String serverType,String serverID,String state ,String curStartTime
+                    ,String core,String mem,String disk,String wJobs,String rJobs ){
+        this.serverType = serverType;
+        this.serverID = Integer.parseInt(serverID);
+        switch (state){
+            case "inactive":
+                this.state =0;
+                break;
+            case "booting":
+                this.state =1;
+                break;
+            case "idle":
+                this.state =2;
+                break;
+            case "active":
+                this.state =3;
+                break;
+            case "unavailable":
+                this.state =4;
+                break;
+        }
+        this.curStartTime = Integer.parseInt(curStartTime);
+        this.core = Integer.parseInt(core);
+        this.mem = Integer.parseInt(mem);
+        this.disk = Long.parseLong(disk);
+        this.wJobs = Integer.parseInt(wJobs);
+        this.rJobs = Integer.parseInt(rJobs);
+    }
 }
